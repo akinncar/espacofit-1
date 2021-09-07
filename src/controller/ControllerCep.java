04e86d1809a6e765376d7d6a9c96b89242525d48
Akinn Rosa <akinncar@hotmail.com>
2021-09-07 18:56:45 -0300

    feat: init

---
+++ src/controller/ControllerCep.java
@@ -0,0 +1,181 @@
+package controller;
+
+import controller.ControllerBcep;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFormattedTextField;
+import javax.swing.JTextField;
+import model.bo.Bairro;
+import model.bo.Cep;
+import model.bo.Cidade;
+import service.BairroService;
+import service.CidadeService;
+import view.TelaBuscaCep;
+import view.TelaCadastroCep;
+
+public class ControllerCep implements ActionListener {
+   TelaCadastroCep tela;
+   public static int codigo;
+   
+   public ControllerCep(TelaCadastroCep telaCadastroCep) {
+      tela = telaCadastroCep;
+      
+      tela.getNovo().addActionListener(this);
+      tela.getCancelar().addActionListener(this);
+      tela.getBuscar().addActionListener(this);
+      tela.getSair().addActionListener(this);
+      tela.getGravar().addActionListener(this);
+
+
+      tela.getListaBairros().setEnabled(false);
+      tela.getListaCidade().setEnabled(false);
+
+      Ativa(true);
+      
+      //combobox
+      tela.getListaCidade().removeAll();
+      tela.getListaCidade().addItem(" - ");
+      for(Cidade cidade : CidadeService.Listar()){
+         tela.getListaCidade().addItem(cidade.getDescricao());
+      }
+      
+      tela.getListaBairros().removeAll();
+      tela.getListaBairros().addItem(" - ");
+      for(Bairro bairro : BairroService.Listar()){
+         tela.getListaBairros().addItem(bairro.getDescricao());
+      }
+      
+      LimpaEstadoComponentes(false);
+      
+      if(codigo != 0){
+            Ativa(false);
+            LimpaEstadoComponentes(true);
+            Cep cep = service.CepService.Listar(codigo);
+            tela.getTextoCep().setText(cep.getCep());
+            tela.getTextoLogradouro().setText(cep.getLogradouro());
+            tela.getTextoObs().setText(cep.getObs());
+            tela.getListaCidade().setSelectedItem(cep.getCidade().getDescricao());
+            tela.getListaBairros().setSelectedItem(cep.getBairro().getDescricao());
+      }
+   }
+   
+   @Override
+   public void actionPerformed(ActionEvent e) {
+      if(e.getSource() == tela.getNovo()){
+         tela.getListaBairros().setEnabled(true);
+         tela.getListaCidade().setEnabled(true);
+         Ativa(false);
+         LimpaEstadoComponentes(true);
+         codigo = 0;
+      }
+      
+      if(e.getSource() == tela.getCancelar()){
+         Ativa(true);
+         tela.getListaBairros().setEnabled(false);
+         tela.getListaCidade().setEnabled(false);
+         tela.getListaBairros().setSelectedItem(" - ");
+         tela.getListaBairros().setSelectedIndex(0);
+         tela.getListaCidade().setSelectedItem(" - ");
+         tela.getListaCidade().setSelectedIndex(0);
+         LimpaEstadoComponentes(false);
+      }
+      
+      if(e.getSource() == tela.getGravar()){
+         Cep cep = new Cep();
+         String string = tela.getTextoCep().getText();
+         String nova = string.replace("-", "");
+         cep.setCep(nova);
+         cep.setLogradouro(tela.getTextoLogradouro().getText());
+         cep.setObs(tela.getTextoObs().getText());
+
+         int idC = 0;
+         
+         for(Cidade cidade : CidadeService.Listar()){
+            System.out.println(cidade.getDescricao());
+            System.out.println(tela.getListaCidade().getSelectedItem());
+            if(cidade.getDescricao().equals(tela.getListaCidade().getSelectedItem())){
+               idC = cidade.getId();
+            }
+         }
+         
+         Cidade cidade = CidadeService.Listar(idC);
+         cep.setCidade(cidade);
+
+         int idB = 0;
+         
+         for(Bairro bairro : BairroService.Listar()){
+            if(bairro.getDescricao().equals(tela.getListaBairros().getSelectedItem())){
+               idB = bairro.getId();
+            }
+         }
+         
+         Bairro bairro = BairroService.Listar(idB);
+         cep.setBairro(bairro);
+         
+         if(codigo == 0){
+            service.CepService.Incluir(cep);
+         }else{
+            cep.setId(service.CepService.Listar(codigo).getId());
+            service.CepService.Atualizar(cep);
+         }
+         
+         Ativa(true);
+         LimpaEstadoComponentes(false);
+
+         tela.getListaBairros().setEnabled(false);
+         tela.getListaCidade().setEnabled(false);
+         tela.getListaBairros().setSelectedItem(" - ");
+         tela.getListaBairros().setSelectedIndex(0);
+         tela.getListaCidade().setSelectedItem(" - ");
+         tela.getListaCidade().setSelectedIndex(0);
+      }
+      
+      if(e.getSource() == tela.getBuscar()){
+            view.TelaBuscaCep telaBusca = new TelaBuscaCep(null, true);
+         new ControllerBcep(telaBusca);
+         telaBusca.setVisible(true);
+         
+         if(codigo != 0){
+            Ativa(false);
+            LimpaEstadoComponentes(true);
+            tela.getListaCidade().setEnabled(true);
+            tela.getListaBairros().setEnabled(true);
+            Cep cep = service.CepService.Listar(codigo);
+            tela.getTextoCep().setText(cep.getCep());
+            tela.getTextoLogradouro().setText(cep.getLogradouro());
+            tela.getTextoObs().setText(cep.getObs());
+            tela.getListaCidade().setSelectedItem(cep.getCidade().getDescricao());
+            tela.getListaBairros().setSelectedItem(cep.getBairro().getDescricao());
+         }
+      }
+      
+      if(e.getSource() == tela.getSair()){
+         tela.dispose();
+      }
+   }
+   
+   //mexer nos botões
+   public void Ativa(boolean estado){
+      tela.getNovo().setEnabled(estado);
+      tela.getCancelar().setEnabled(!estado);
+      tela.getBuscar().setEnabled(estado);
+      tela.getSair().setEnabled(estado);
+      tela.getGravar().setEnabled(!estado);
+   }
+   
+   public void LimpaEstadoComponentes(boolean estado){
+      Component[] componentes = tela.getCenter().getComponents();
+      for(Component componente : componentes){
+         if(componente instanceof JTextField){
+            ((JTextField) componente).setText("");
+            componente.setEnabled(estado);
+         }
+         
+         if(componente instanceof JFormattedTextField){
+            ((JFormattedTextField) componente).setValue(null);
+            componente.setEnabled(estado);
+         }
+      }
+   }
+}
